67f4671ef29d61342b23a445808b23454ee5c078
Travis Mitchell <contact@traviscmitchell.com>
2015-04-21 22:04:08 -0700

    Cleanup, simplified selectors.

--- todoMvcTest.js
+++ todoMvcTest.js
@@ -1,9 +1,8 @@
 var webdriverio = require('webdriverio');
 var chai = require('chai');
-var assert = chai.assert;
 var chaiAsPromised = require('chai-as-promised');
-var H = require('./helpers')
-var Q = require('Q')
+var H = require('./helpers');
+var Q = require('Q');
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -22,29 +21,36 @@ function pressButton(submitKey) {
     client.addValue(selector, submitKey, cb);
   }
 }
+
 client.addCommand("pressReturn", pressButton('\uE006'));
 client.addCommand("pressEnter", pressButton('\uE007'));
 
-function resetBrowser() {
-  client.end();
-  return client.init().url(appUrl).waitFor('#todoapp', 5000);
-}
+function initBrowser() { return client.init().url(appUrl).waitFor('#todoapp', 5000); }
+
+function killBrowser() { return client.end(); }
+
+function resetBrowser() { return client.end().init().url(appUrl).waitFor('#todoapp', 5000); }
 
 describe('Todos', function() {
 
-  before(function() { return client.init().url(appUrl).waitFor('#todoapp', 5000); });
-
-  after(function() { return client.end(); });
+  before(initBrowser);
+  after(killBrowser);
 
   describe('The page elements', function() {
     it('should have the header "todos"', function() {
-      return client.getText('header#header > h1').should.become('todos')
+      return client
+        .getText('header#header > h1')
+        .should.eventually.equal('todos');
     });
     it('should have an input with the correct placeholder', function() {
-      return client.getAttribute('input#new-todo', 'placeholder').should.eventually.equal('What needs to be done?')
+      return client
+        .getAttribute('#new-todo', 'placeholder')
+        .should.eventually.equal('What needs to be done?');
     });
     it('should have instructions', function() {
-      return client.getText('footer#info > p:first-child').should.eventually.equal('Double-click to edit a todo')
+      return client
+        .getText('footer#info > p:first-child')
+        .should.eventually.equal('Double-click to edit a todo');
     });
   });
 
@@ -52,55 +58,60 @@ describe('Todos', function() {
     beforeEach(resetBrowser);
     it('should add an item to the todo list if text is entered and return is pressed', function() {
       var randomString = H.getRandomString();
-      return client.setValue('input#new-todo', randomString)
-          .pressReturn('input#new-todo')
-          .getText('ul#todo-list > li').should.eventually.equal(randomString);
+      return client.setValue('#new-todo', randomString)
+          .pressReturn('#new-todo')
+          .getText('#todo-list li')
+          .should.eventually.equal(randomString);
     });
     it('should add an item to the todo list if text is entered and enter is pressed', function() {
       var randomString = H.getRandomString();
-      return client.setValue('input#new-todo', randomString)
-          .pressEnter('input#new-todo')
-          .getText('ul#todo-list > li').should.eventually.equal(randomString);
+      return client
+        .setValue('#new-todo', randomString)
+        .pressEnter('#new-todo')
+        .getText('#todo-list li')
+        .should.eventually.equal(randomString);
     });
     it('should not add an item to the todo list if no text is entered', function() {
       return client
-        .pressReturn('input#new-todo')
-        .getText('ul#todo-list').should.eventually.equal('')
+        .pressReturn('#new-todo')
+        .getText('#todo-list').should.eventually.equal('')
     });
     it('should add new items after the existing ones', function() {
       var randomTodos = H.getRandomStrings(10);
       return Q.all(randomTodos
         .map(function(todo) {
           return client
-            .setValue('input#new-todo', todo)
-            .pressReturn('input#new-todo')
+            .setValue('#new-todo', todo)
+            .pressReturn('#new-todo');
         })
         .map(function(todo, i) {
           return client
-            .getText('ul#todo-list > li:nth-child(' + (i+1) + ')')
-            .should.eventually.equal(randomTodos[i])
+            .getText('#todo-list li:nth-child(' + (i+1) + ')')
+            .should.eventually.equal(randomTodos[i]);
         })
       );
     });
-  })
+  });
 
   describe('Checking a todo', function() {
     beforeEach(resetBrowser);
     it('should cross off a todo', function() {
       return client
-        .setValue('input#new-todo', H.getRandomString())
-        .pressReturn('input#new-todo')
-        .click('ul#todo-list > li input.toggle')
-        .getAttribute('ul#todo-list > li', 'class').should.eventually.contain('completed')
+        .setValue('#new-todo', H.getRandomString())
+        .pressReturn('#new-todo')
+        .click('#todo-list li .toggle')
+        .getAttribute('#todo-list li', 'class')
+        .should.eventually.contain('completed');
     });
     it('should make the todo visible on the "completed" list', function() {
       var randomString = H.getRandomString()
       return client
-        .setValue('input#new-todo', randomString)
-        .pressReturn('input#new-todo')
-        .click('ul#todo-list > li input.toggle')
+        .setValue('#new-todo', randomString)
+        .pressReturn('#new-todo')
+        .click('#todo-list li .toggle')
         .click('#filters > li:nth-child(3)')
-        .getText('ul#todo-list > li').should.eventually.equal(randomString);
+        .getText('#todo-list li')
+        .should.eventually.equal(randomString);
     });
   });
 
@@ -110,9 +121,9 @@ describe('Todos', function() {
     });
     it('should remove a todo from the list', function() {
       return client
-        .setValue('input#new-todo', H.getRandomString())
-        .pressReturn('input#new-todo')
-        .click('ul#todo-list li .destroy');
+        .setValue('#new-todo', H.getRandomString())
+        .pressReturn('#new-todo')
+        .click('#todo-list li .destroy');
     });
   });
 
